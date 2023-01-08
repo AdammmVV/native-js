d7e709786187094de84ca410f02d920597f72b10
Adammm <adammm.web@gmail.com>
2023-01-08 23:51:58 +0300

    create test

--- src/1-hello-test/01.test.ts
+++ src/1-hello-test/01.test.ts
@@ -1,4 +1,4 @@
-import {summ} from "./01";
+import {mult, summ} from "./01";
 
 test('sum should be correct', () => {
     //data
@@ -7,8 +7,25 @@ test('sum should be correct', () => {
     const c = 3;
 
     //action
-    const result = summ(a,b);
+    const result1 = summ(a,b);
+    const result2 = summ(b,c);
 
     //expect result
-    expect(result).toBe(3)
+    expect(result1).toBe(3)
+    expect(result2).toBe(5)
+})
+
+test('multiply should be correct', () => {
+    //data
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+    //action
+    const result1 = mult(a,b);
+    const result2 = mult(b,c);
+
+    //expect result
+    expect(result1).toBe(2)
+    expect(result2).toBe(6)
 })
